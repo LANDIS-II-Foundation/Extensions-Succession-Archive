08e8c11bc34a8f4b046e7986844662611cc958ea
Rob <rmscheller@gmail.com>
2013-06-24 15:46:57 +0000

    works with new clmate-generator
    git-svn-id: http://Marc-PC/svn/Full@1206 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/Century-succession/branches/amin-century/utility/Util.cs
@@ -0,0 +1,31 @@
+//  Copyright 2007-2010 Portland State University, University of Wisconsin-Madison
+//  Author: Robert Scheller, Ben Sulman
+
+using Landis.Core;
+using Landis.SpatialModeling;
+using Landis.Library.LeafBiomassCohorts;
+
+namespace Landis.Extension.Succession.Century
+{
+    /// <summary>
+    /// Utility methods.
+    /// </summary>
+    public static class Util
+    {
+
+        /// <summary>
+        /// Converts a table indexed by species and ecoregion into a
+        /// 2-dimensional array.
+        /// </summary>
+        public static T[,] ToArray<T>(Species.AuxParm<Ecoregions.AuxParm<T>> table)
+        {
+            T[,] array = new T[PlugIn.ModelCore.Ecoregions.Count, PlugIn.ModelCore.Species.Count];
+            foreach (ISpecies species in PlugIn.ModelCore.Species) {
+                foreach (IEcoregion ecoregion in PlugIn.ModelCore.Ecoregions) {
+                    array[ecoregion.Index, species.Index] = table[species][ecoregion];
+                }
+            }
+            return array;
+        }
+    }
+}
