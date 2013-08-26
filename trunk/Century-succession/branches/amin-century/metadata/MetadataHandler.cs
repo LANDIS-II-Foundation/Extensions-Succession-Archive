b2079ef7a4dc34e8de0d7ebce0a17799c7a2d347
Rob <rmscheller@gmail.com>
2013-08-26 17:20:30 +0000

    Added Metadata folder to the Repo
    git-svn-id: http://Marc-PC/svn/Full@1245 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/Century-succession/branches/amin-century/metadata/MetadataHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Data;
+using System.Text;
+using Landis.Library.Metadata;
+
+namespace Landis.Extension.Succession.Century
+{
+    public static class MetadataHandler
+    {
+        
+        public static ExtensionMetadata Extension {get; set;}
+
+        public static void InitializeMetadata()
+        {
+            ScenarioReplicationMetadata scenRep = new ScenarioReplicationMetadata() {
+                FolderName = "Scen_?-rep_?", //we should probably add this to the extension/scenario input file or we might be leaving this out because the extensions do not need to know anything about the replication (the hirarchy of the scenario-replications and their extensions are defined by the convention of folder structures)
+                RasterOutCellArea = PlugIn.ModelCore.CellArea,
+                TimeMin = PlugIn.ModelCore.StartTime,
+                TimeMax = PlugIn.ModelCore.EndTime,
+                ProjectionFilePath = "Projection.?" //we should probably add this to the extension/scenario input file
+            };
+
+            Extension = new ExtensionMetadata(){
+                Name = "Century-Succession",
+                TimeInterval = PlugIn.ModelCore.TimeSinceStart, //TimeInterval?
+                ScenarioReplicationMetadata = scenRep
+            };
+
+            //---------------------------------------
+            //          table outputs:   
+            //---------------------------------------
+
+            OutputMetadata tblOut_monthly = new OutputMetadata()
+            {
+                Type = OutputType.Table,
+                Name = "MonthlyLog",
+                FilePath = Outputs.dtMonthly.FilePath,
+                MetadataFilePath = @"Century-Succession\MonthlyLog.xml"
+            };
+            tblOut_monthly.RetriveFields(typeof(MonthlyLog));
+            Extension.OutputMetadatas.Add(tblOut_monthly);
+
+
+            //---------------------------------------            
+            //          map outputs:         
+            //---------------------------------------
+
+            OutputMetadata mapOut_ANPP = new OutputMetadata()
+            {
+                Type = OutputType.Map,
+                Name = "ag_npp",
+                FilePath = @"Century-Succession\ag_npp-1882.img"
+                //MetadataFilePath = @"Century-Succession\ANPP.xml"
+            };
+            Extension.OutputMetadatas.Add(mapOut_ANPP);
+
+            //---------------------------------------
+            MetadataProvider mp = new MetadataProvider(Extension);
+            mp.WriteMetadataToXMLFile("Metadata", Extension.Name, Extension.Name);
+
+
+
+
+        }
+    }
+}
---
+++ trunk/Century-succession/branches/amin-century/metadata/MonthlyLog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Landis.Library.Metadata;
+
+namespace Landis.Extension.Succession.Century
+{
+    public class MonthlyLog
+    {
+        [DataFieldAttribute(Unit = FiledUnits.Year, Desc = "...")]
+        public int Time {set; get;}
+
+        [DataFieldAttribute(Unit = FiledUnits.Month, Desc = "...")]
+        public int Month { set; get; }
+
+        [DataFieldAttribute(Unit = FiledUnits.None, Desc = "...")]
+        public string EcoregionName { set; get; }
+
+        [DataFieldAttribute(Unit = FiledUnits.None, Desc = "...")]
+        public int NumSites { set; get; }
+
+        [DataFieldAttribute(Unit = FiledUnits.g_C_m_2, Desc = "...", Format = "{1:0.0}")]
+        public double ppt {get; set;}// = new double[PlugIn.ModelCore.Ecoregions.Count];
+
+        [DataFieldAttribute(Unit = FiledUnits.g_C_m_2, Desc = "...", Format = "{1:0.0}")]
+        public double airtemp { get; set; }// = new double[PlugIn.ModelCore.Ecoregions.Count];
+
+        [DataFieldAttribute(Unit = FiledUnits.g_C_m_2, Desc = "...", Format = "{1:0.0}")]
+        public double avgNPPtc { get; set; }// = new double[PlugIn.ModelCore.Ecoregions.Count];
+
+        [DataFieldAttribute(Unit = FiledUnits.g_C_m_2, Desc = "...", Format = "{1:0.0}")]
+        public double avgResp { get; set; }// = new double[PlugIn.ModelCore.Ecoregions.Count];
+
+        [DataFieldAttribute(Unit = FiledUnits.g_C_m_2, Desc = "...", Format = "{1:0.0}")]
+        public double avgNEE { get; set; }// = new double[PlugIn.ModelCore.Ecoregions.Count];
+
+        [DataFieldAttribute(Unit = FiledUnits.g_C_m_2, Desc = "...", Format = "{1:0.0}")]
+        public double Ndep { get; set; }// = new double[PlugIn.ModelCore.Ecoregions.Count];
+    }
+}
