566025a008d92fc315c557f2dbbd75ad12782cca
Rob <rmscheller@gmail.com>
2013-11-04 19:16:57 +0000

    Minor tweaks to get the log files working for single cells.
    git-svn-id: http://Marc-PC/svn/Full@1366 85f15e0e-5137-b144-a863-98bd542365a3

--- trunk/Century-succession/branches/amin-century/Outputs.cs
+++ trunk/Century-succession/branches/amin-century/Outputs.cs
@@ -86,6 +86,8 @@ namespace Landis.Extension.Succession.Century
         //---------------------------------------------------------------------
         public static void WritePrimaryLogFile(int CurrentTime)
         {
+
+            PlugIn.ModelCore.UI.WriteLine("Stuff happening!");
             
             double[] avgAnnualPPT  = new double[PlugIn.ModelCore.Ecoregions.Count];
             double[] avgJJAtemp    = new double[PlugIn.ModelCore.Ecoregions.Count];
